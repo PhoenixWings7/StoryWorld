11e8fd688a8c5b0ac9dada5f5af287e01390564f
PhoenixWings7 <adamus.klaudia.pasp@gmail.com>
2024-06-19 16:24:46 +0200

    add place types

--- src/main/java/pl/edu/pjwstk/s24987/model/Place.java
+++ src/main/java/pl/edu/pjwstk/s24987/model/Place.java
@@ -1,11 +1,90 @@
 package pl.edu.pjwstk.s24987.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.OneToMany;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity(name = "places")
 public class Place extends WorldElement {
+    @Enumerated
+    private PlaceType type;
+    @OneToMany
+    private List<Place> primaryPlaces = new ArrayList<>();
+    @OneToMany
+    private List<Place> secondaryPlaces = new ArrayList<>();
+
     public Place() {
         super("New place");
     }
 
     public Place(String name) {
         super(name);
+    }
+
+    public PlaceType getType() {
+        return type;
+    }
+
+    public void setType(PlaceType type) {
+        this.type = type;
+    }
+
+    public List<Place> getPrimaryPlaces() {
+        return primaryPlaces;
+    }
+
+    public void setPrimaryPlaces(List<Place> primaryPlaces) {
+        this.primaryPlaces = primaryPlaces;
+    }
+
+    public List<Place> getSecondaryPlaces() {
+        return secondaryPlaces;
+    }
+
+    public void setSecondaryPlaces(List<Place> secondaryPlaces) {
+        this.secondaryPlaces = secondaryPlaces;
+    }
+
+    public void linkPrimaryPlace(Place primaryPlace) {
+        primaryPlaces.add(primaryPlace);
+        primaryPlace.linkSecondaryPlace(this);
+        reevaluatePlaceType();
+    }
+
+    public void unlinkPrimaryPlace(Place primaryPlace) {
+        primaryPlaces.remove(primaryPlace);
+        primaryPlace.unlinkSecondaryPlace(this);
+        reevaluatePlaceType();
+    }
+
+    public void linkSecondaryPlace(Place secondaryPlace) {
+        secondaryPlaces.add(secondaryPlace);
+        secondaryPlace.linkPrimaryPlace(this);
+        reevaluatePlaceType();
+    }
+
+    public void unlinkSecondaryPlace(Place secondaryPlace) {
+        secondaryPlaces.remove(secondaryPlace);
+        secondaryPlace.unlinkPrimaryPlace(this);
+        reevaluatePlaceType();
+    }
+
+    private void reevaluatePlaceType() {
+        int primaryPlacesNum = primaryPlaces.size();
+        int secondaryPlacesNum = secondaryPlaces.size();
+        if (primaryPlacesNum == 0 && secondaryPlacesNum > 0) {
+            type = PlaceType.PRIMARY;
+        } else if (primaryPlacesNum > 0 && secondaryPlacesNum == 0) {
+            type = PlaceType.SECONDARY;
+        } else {
+            type = PlaceType.MIDDLE;
+        }
+    }
+
+    public enum PlaceType {
+        PRIMARY, MIDDLE, SECONDARY
     }
 }
