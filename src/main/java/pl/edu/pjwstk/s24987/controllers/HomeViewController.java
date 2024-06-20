bbc9098cc6d6af2b18111abf29464fd7a032db5e
PhoenixWings7 <adamus.klaudia.pasp@gmail.com>
2024-06-20 16:03:39 +0200

    implement displaying worlds list

--- src/main/java/pl/edu/pjwstk/s24987/controllers/HomeViewController.java
+++ src/main/java/pl/edu/pjwstk/s24987/controllers/HomeViewController.java
@@ -1,12 +1,22 @@
 package pl.edu.pjwstk.s24987.controllers;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.ListView;
+import pl.edu.pjwstk.s24987.data.StoryWorldDao;
+import pl.edu.pjwstk.s24987.data.StoryWorldDaoImpl;
 
-public class HomeViewController {
+import java.net.URL;
+import java.util.List;
+import java.util.ResourceBundle;
 
+public class HomeViewController implements Initializable {
+    private final StoryWorldDao storyWorldDao = new StoryWorldDaoImpl();
+    ObservableList<String> worldNames = FXCollections.observableArrayList();
     @FXML
     private Button accountBtn;
 
@@ -20,7 +30,13 @@ public class HomeViewController {
     private Button newWorldBtn;
 
     @FXML
-    private ListView<?> worldsListView;
+    private ListView<String> worldsListView;
 
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        List<String> names = storyWorldDao.getWorldNames();
+        worldNames.setAll(names);
+        worldsListView.setItems(worldNames);
+    }
 }
 
